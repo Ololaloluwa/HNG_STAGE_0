d5c0c66edd7cd79d528aa73a893df8132f6c406a
Ololaloluwa Olojede Ethan <olola1720@gmail.com>
2025-01-31 11:57:26 +0100

    added main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app.add_middleware(
 
 @app.get("/" ,status_code = status.HTTP_200_OK )
 def user_info():
-  date_time = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S")
+  date_time = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%SZ")
   return {
   "email": "olola1720@gmail.com",
   "current_datetime":  date_time,
